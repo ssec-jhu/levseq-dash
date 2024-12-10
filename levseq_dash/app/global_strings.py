edd52c6ad871245e4d3827356bb44749b7387901
Fatemeh <1739327+fabbasinejad@users.noreply.github.com>
2024-12-10 15:16:52 -0500

    drafting upload form for testing

---
+++ levseq_dash/app/global_strings.py
@@ -0,0 +1,9 @@
+# app strings
+web_title = "LevSeq Dashboard"  # TODO: is this good?
+
+# form strings
+experiment_name = "Experiment Name"
+experiment_name_placeholder = "Enter a name for your experiment."
+
+# DO NOT CHANGE
+dbc_template_name = "flatly"
---
+++ levseq_dash/app/main_app.py
@@ -0,0 +1,32 @@
+import dash_bootstrap_components as dbc
+from dash import Dash, html
+from dash_bootstrap_templates import load_figure_template
+
+from levseq_dash.app import layout_upload
+from levseq_dash.app import global_strings as gs
+
+# Initialize the app
+dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
+app = Dash(
+    __name__,
+    title=gs.web_title,
+    external_stylesheets=[dbc.themes.FLATLY, dbc_css, dbc.icons.BOOTSTRAP, dbc.icons.FONT_AWESOME],
+)
+
+# VERY important line of code for running with gunicorn
+# you run the 'server' not the 'app'. VS. you run the 'app' with uvicorn
+server = app.server
+
+load_figure_template(gs.dbc_template_name)
+# Define the form layout
+
+
+app.layout = dbc.Container(
+    [
+        layout_upload.upload_form_layout
+    ],
+    # fluid=True,
+)
+# Run the app
+if __name__ == "__main__":
+    app.run()
