483ce1d47689b08d10fcf350f3ef34a5ba7322e6
Fatemeh <1739327+fabbasinejad@users.noreply.github.com>
2025-01-27 09:56:02 -0500

    adding the web service api to the directory with settings file to hadnle the host

---
+++ levseq_dash/app/wsexec.py
@@ -0,0 +1,72 @@
+#
+# wsexec.py
+#
+
+import re
+
+import requests
+
+from levseq_dash.app.settings import CONFIG
+
+# import globals as g
+
+# handy type aliases
+type Scalar = int | float | str
+type Rowset = list[tuple]
+type Columns = list[str]
+type ResultSet = tuple[Columns, Rowset]
+type QueryResponse = ResultSet | Scalar
+type Arglist = list[Scalar]
+
+
+lswsurl = f'{CONFIG["db-service"]["host"]}:{CONFIG["db-service"]["port"]}'
+
+
+# define a class that supports json serialization of HTTP request parameters
+class QueryPackage(dict):
+    verb: str
+    params: list
+
+    def __new__(cls, verb, params):
+        instance = super(QueryPackage, cls).__new__(cls)
+        return instance
+
+    def __init__(self, verb, params):
+        super().__init__(verb=verb, params=params)
+
+        self.verb = verb
+        self.params = params
+
+
+# HTTP POST to the LevSeq webservice
+#
+def Query(verb: str, params: list[Scalar]) -> QueryResponse:
+    # sanity check parameters
+    if not isinstance(params, list) or not all(isinstance(p, (int, float, str)) for p in params):
+        raise ValueError("query parameters are not list[Scalar]")
+
+    # HTTP request/response
+    pkg = QueryPackage(verb, params)
+    resp = requests.post(lswsurl, json=pkg)
+    if not resp.ok:
+        msg = f"LevSeq webservice response: {resp.status_code} {resp.reason}"
+        if resp.text != "null":
+            j = resp.json()
+            msg += f": \n{j["detail"]}"
+
+        raise ValueError(msg)
+
+    # use the verb prefix to determine the type of the result set
+    m = re.match(r"^(\w+?)_", verb)
+    if m is None:
+        raise ValueError(f"invalid request verb '{verb}'")
+
+    # deserialize the json-formatted response
+    j = resp.json()
+
+    # "get_" queries return a ResultSet
+    if m[1] == "get":
+        return j["columns"], j["rows"]
+
+    # all other queries return a Scalar (regardless of how the LevSeq webservice handles them)
+    return j["details"]
