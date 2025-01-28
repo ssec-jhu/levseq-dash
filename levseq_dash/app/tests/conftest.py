fe4749652efddfc2546f0fd22a62ded041bf5c73
Fatemeh <1739327+fabbasinejad@users.noreply.github.com>
2025-01-28 15:41:50 -0500

    wip

---
+++ levseq_dash/app/tests/conftest.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import pandas as pd
+
+from levseq_dash.app.experiment import Experiment
+
+package_root = Path(__file__).resolve().parent.parent.parent
+
+path_assay = package_root / "app" / "tests" / "data" / "assay_measure_list.csv"
+path_exp_ep_data = package_root / "app" / "tests" / "data" / "flatten_ep_processed_xy_cas.csv"
+path_exp_ep_cif = package_root / "app" / "tests" / "data" / "flatten_ep_processed_xy_cas_row8.cif"
+
+path_exp_ssm_data = package_root / "app" / "tests" / "data" / "flatten_ssm_processed_xy_cas.csv"
+path_exp_ssm_cif = package_root / "app" / "tests" / "data" / "flatten_ssm_processed_xy_cas_row3.cif"
+
+
+test_assay_df = (pd.read_csv(path_assay, encoding="utf-8", usecols=["Technique"]))["Technique"].tolist()[2]
+
+experiment_ep_example = Experiment(
+    data_df=pd.read_csv(path_exp_ep_data),
+    experiment_name="ep_file",
+    experiment_date="TBD",
+    mutagenesis_method="epPcR",
+    geometry_file=path_exp_ep_cif,
+    assay=test_assay_df,
+)
+
+experiment_ssm_example = Experiment(
+    data_df=pd.read_csv(path_exp_ssm_data),
+    experiment_name="ssm_file",
+    experiment_date="TBD",
+    mutagenesis_method="SSM",
+    geometry_file=path_exp_ssm_cif,
+    assay=test_assay_df,
+)
