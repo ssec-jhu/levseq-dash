0a40632bbb3dbf54ce279846438c1468f125088f
Fatemeh <1739327+fabbasinejad@users.noreply.github.com>
2025-06-11 19:19:56 -0400

    Fixed similar sequences button spinner and clear results (#128)

--- levseq_dash/app/components/layout/layout_matching_sequences.py
+++ levseq_dash/app/components/layout/layout_matching_sequences.py
@@ -1,5 +1,5 @@
 import dash_bootstrap_components as dbc
-from dash import html
+from dash import dcc, html
 
 from levseq_dash.app import global_strings as gs
 from levseq_dash.app import global_strings_html as gsh
@@ -144,6 +144,149 @@ def create_layout_reaction(id_image, id_substrate_smiles, id_product_smiles):
     )
 
 
+def get_se_alignment_results_layout():
+    return (
+        html.Div(
+            id="id-div-seq-alignment-results",
+            style=vis.display_none,
+            children=[
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                dbc.Card(
+                                    [
+                                        dbc.CardHeader(gs.seg_align_results, className=vis.top_card_head),
+                                        dbc.CardBody(
+                                            [
+                                                dbc.Row(
+                                                    [
+                                                        # this info icon uses markdown in the tooltip so we
+                                                        # must allow html and set the flag to true
+                                                        html.Div(
+                                                            [
+                                                                widgets.get_info_icon_tooltip_bundle(
+                                                                    info_icon_id="id-info-1",
+                                                                    help_string=gsh.markdown_note_matched_seq,
+                                                                    location="top",
+                                                                    allow_html=True,
+                                                                ),
+                                                                html.P(
+                                                                    id="id-div-matched-sequences-info",
+                                                                    className="fw-bolder",
+                                                                ),
+                                                            ],
+                                                            style={"display": "flex", "gap": "5px"},
+                                                        ),
+                                                    ],
+                                                ),
+                                                dbc.Row(
+                                                    [widgets.get_table_matched_sequences()],
+                                                    className="dbc dbc-ag-grid",
+                                                ),
+                                            ],
+                                            className="p-1 mt-3",  # fits to the card border
+                                        ),
+                                    ],
+                                    style={
+                                        "box-shadow": "1px 2px 7px 0px grey",
+                                        "border-radius": "5px",
+                                        "height": vis.seq_match_card_height,
+                                    },
+                                ),
+                            ],
+                            width=7,
+                            style=vis.border_column,
+                            # remove all gutters from the col to snap to the card
+                            # className="g-3"
+                        ),
+                        dbc.Col(
+                            [
+                                dbc.Card(
+                                    [
+                                        dbc.CardHeader(gs.seq_align_visualize, className=vis.top_card_head),
+                                        dbc.CardBody(
+                                            [
+                                                dbc.Row(
+                                                    [
+                                                        create_layout_reaction(
+                                                            "id-selected-seq-matched-reaction-image",
+                                                            "id-selected-seq-matched-substrate",
+                                                            "id-selected-seq-matched-product",
+                                                        )
+                                                    ],
+                                                    className="mb-3",
+                                                ),
+                                                dbc.Row([html.Div(id="id-viewer-selected-seq-matched-protein")]),
+                                            ],
+                                            className="p-1 mb-3",  # fits to the card border
+                                        ),
+                                    ],
+                                    style={
+                                        "box-shadow": "1px 2px 7px 0px grey",
+                                        "border-radius": "5px",
+                                        "height": vis.seq_match_card_height,
+                                    },
+                                ),
+                            ],
+                            style=vis.border_column,
+                        ),
+                    ],
+                    className="g-2 mt-4 mb-4",
+                ),
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                dbc.Card(
+                                    [
+                                        dbc.CardHeader(gs.seq_align_residues, className=vis.top_card_head),
+                                        dbc.CardBody(
+                                            [
+                                                dbc.Row(
+                                                    [
+                                                        dbc.Col(
+                                                            widgets.get_button_download(
+                                                                "id-button-download-hot-cold-results"
+                                                            ),
+                                                            width=2,
+                                                            align="center",
+                                                            style=vis.border_column,
+                                                        ),
+                                                        dbc.Col(
+                                                            widgets.get_radio_items_download_options(
+                                                                "id-button-download-hot-cold-results-options"
+                                                            ),
+                                                            width=3,
+                                                            align="center",
+                                                            style=vis.border_column,
+                                                        ),
+                                                    ],
+                                                    className="mb-2 g-1",
+                                                ),
+                                                dbc.Row(
+                                                    [widgets.get_table_matched_sequences_exp_hot_cold_data()],
+                                                    className="dbc dbc-ag-grid",
+                                                    # style=vis.border_table,
+                                                ),
+                                            ],
+                                            className="p-1 mt-3",  # fits to the card border
+                                        ),
+                                    ],
+                                    className="d-flex flex-column",  # Flexbox for vertical stacking
+                                    style=vis.card_shadow,
+                                ),
+                            ],
+                            style=vis.border_column,
+                        ),
+                    ],
+                    className="mt-4 mb-4",
+                ),
+            ],
+        ),
+    )
+
+
 def get_seq_align_layout():
     return html.Div(
         [
@@ -165,144 +308,16 @@ def get_seq_align_layout():
                 ],
             ),
             # these results will appear/clear
-            html.Div(
-                id="id-div-seq-alignment-results",
-                # TODO: integrate this with clearing results boolean later
-                # style={**vis.section_vis},
-                children=[
-                    dbc.Row(
-                        [
-                            dbc.Col(
-                                [
-                                    dbc.Card(
-                                        [
-                                            dbc.CardHeader(gs.seg_align_results, className=vis.top_card_head),
-                                            dbc.CardBody(
-                                                [
-                                                    dbc.Row(
-                                                        [
-                                                            # this info icon uses markdown in the tooltip so we
-                                                            # must allow html and set the flag to true
-                                                            html.Div(
-                                                                [
-                                                                    widgets.get_info_icon_tooltip_bundle(
-                                                                        info_icon_id="id-info-1",
-                                                                        help_string=gsh.markdown_note_matched_seq,
-                                                                        location="top",
-                                                                        allow_html=True,
-                                                                    ),
-                                                                    html.P(
-                                                                        id="id-div-matched-sequences-info",
-                                                                        className="fw-bolder",
-                                                                    ),
-                                                                ],
-                                                                style={"display": "flex", "gap": "5px"},
-                                                            ),
-                                                        ],
-                                                    ),
-                                                    dbc.Row(
-                                                        [widgets.get_table_matched_sequences()],
-                                                        className="dbc dbc-ag-grid",
-                                                    ),
-                                                ],
-                                                className="p-1 mt-3",  # fits to the card border
-                                            ),
-                                        ],
-                                        style={
-                                            "box-shadow": "1px 2px 7px 0px grey",
-                                            "border-radius": "5px",
-                                            "height": vis.seq_match_card_height,
-                                        },
-                                    ),
-                                ],
-                                width=7,
-                                style=vis.border_column,
-                                # remove all gutters from the col to snap to the card
-                                # className="g-3"
-                            ),
-                            dbc.Col(
-                                [
-                                    dbc.Card(
-                                        [
-                                            dbc.CardHeader(gs.seq_align_visualize, className=vis.top_card_head),
-                                            dbc.CardBody(
-                                                [
-                                                    dbc.Row(
-                                                        [
-                                                            create_layout_reaction(
-                                                                "id-selected-seq-matched-reaction-image",
-                                                                "id-selected-seq-matched-substrate",
-                                                                "id-selected-seq-matched-product",
-                                                            )
-                                                        ],
-                                                        className="mb-3",
-                                                    ),
-                                                    dbc.Row([html.Div(id="id-viewer-selected-seq-matched-protein")]),
-                                                ],
-                                                className="p-1 mb-3",  # fits to the card border
-                                            ),
-                                        ],
-                                        style={
-                                            "box-shadow": "1px 2px 7px 0px grey",
-                                            "border-radius": "5px",
-                                            "height": vis.seq_match_card_height,
-                                        },
-                                    ),
-                                ],
-                                style=vis.border_column,
-                            ),
-                        ],
-                        className="g-2 mt-4 mb-4",
-                    ),
-                    dbc.Row(
-                        [
-                            dbc.Col(
-                                [
-                                    dbc.Card(
-                                        [
-                                            dbc.CardHeader(gs.seq_align_residues, className=vis.top_card_head),
-                                            dbc.CardBody(
-                                                [
-                                                    dbc.Row(
-                                                        [
-                                                            dbc.Col(
-                                                                widgets.get_button_download(
-                                                                    "id-button-download-hot-cold-results"
-                                                                ),
-                                                                width=2,
-                                                                align="center",
-                                                                style=vis.border_column,
-                                                            ),
-                                                            dbc.Col(
-                                                                widgets.get_radio_items_download_options(
-                                                                    "id-button-download-hot-cold-results-options"
-                                                                ),
-                                                                width=3,
-                                                                align="center",
-                                                                style=vis.border_column,
-                                                            ),
-                                                        ],
-                                                        className="mb-2 g-1",
-                                                    ),
-                                                    dbc.Row(
-                                                        [widgets.get_table_matched_sequences_exp_hot_cold_data()],
-                                                        className="dbc dbc-ag-grid",
-                                                        # style=vis.border_table,
-                                                    ),
-                                                ],
-                                                className="p-1 mt-3",  # fits to the card border
-                                            ),
-                                        ],
-                                        className="d-flex flex-column",  # Flexbox for vertical stacking
-                                        style=vis.card_shadow,
-                                    ),
-                                ],
-                                style=vis.border_column,
-                            ),
-                        ],
-                        className="mt-4 mb-4",
-                    ),
-                ],
+            dcc.Loading(
+                overlay_style={"visibility": "visible", "filter": "blur(2px)"},
+                type="circle",
+                color="var(--bs-secondary)",
+                # Use the target_components property to specify which component id and component property
+                # combinations it wraps can trigger the loading spinner.
+                # The spinner component is displayed only when one of the
+                # target component and properties enters loading state.
+                target_components={"id-table-matched-sequences": "rowData"},
+                children=get_se_alignment_results_layout(),
             ),
         ],
     )
