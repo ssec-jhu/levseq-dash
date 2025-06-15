f595ba97f116f14ed95729187a93a31d37d2ea16
Fatemeh <1739327+fabbasinejad@users.noreply.github.com>
2025-06-15 14:46:07 -0400

    fixed formatting

--- levseq_dash/app/assets/dashAgGridFunctions.js
+++ levseq_dash/app/assets/dashAgGridFunctions.js
@@ -4,10 +4,8 @@ dagfuncs.shortenMutagenesisMethod = function(params) {
 
   if (params === 'Site saturation mutagenesis (SSM)') {
     return 'SSM';
-  }
-  else if (params === 'Error-prone PCR (epPCR)') {
+  } else if (params === 'Error-prone PCR (epPCR)') {
     return 'epPCR';
-  }
-  else
+  } else
     return params;
 };
